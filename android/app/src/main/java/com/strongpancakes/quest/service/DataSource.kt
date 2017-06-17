1a7514407f1f77b7b820b82d712c5ddc7cf5ba59
Dima Korolev <karaliou.dzmitryi@gmail.com>
2017-06-17 18:19:13 +0300

    Added models and service for mockup.

---
+++ android/app/src/main/java/com/strongpancakes/quest/service/DataSource.kt
@@ -0,0 +1,16 @@
+package com.strongpancakes.quest.service
+
+import com.strongpancakes.quest.data.profile.Achievement
+import com.strongpancakes.quest.data.profile.User
+import io.reactivex.Observable
+
+/**
+ * Created by dima_korolev on 17/06/2017.
+ */
+
+interface DataSource {
+    fun getMe(): Observable<User>
+    fun getUser(id: String): Observable<User>
+
+    fun getAchievements(): Observable<Achievement>
+}
