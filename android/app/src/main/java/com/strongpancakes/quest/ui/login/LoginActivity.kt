96571ed7478e2f70b7075d2943c7fbb0808c9811
Dima Korolev <karaliou.dzmitryi@gmail.com>
2017-06-17 16:46:41 +0300

    Implemented login + signup. Added activity utils.

--- android/app/src/main/java/com/strongpancakes/quest/ui/login/LoginActivity.kt
+++ android/app/src/main/java/com/strongpancakes/quest/ui/login/LoginActivity.kt
@@ -1,8 +1,20 @@
 package com.strongpancakes.quest.ui.login
 
+import android.content.Intent
 import android.os.Bundle
+import android.support.design.widget.Snackbar
 import android.support.v7.app.AppCompatActivity
+import android.util.Patterns
+import com.jakewharton.rxbinding2.view.RxView
+import com.jakewharton.rxbinding2.widget.RxTextView
+import com.strongpancakes.quest.OfficeMeApp
 import com.strongpancakes.quest.R
+import com.strongpancakes.quest.ui.main.MainActivity
+import com.strongpancakes.quest.utils.start
+import io.reactivex.Observable
+import io.reactivex.functions.BiFunction
+import kotlinx.android.synthetic.main.activity_login.*
+
 
 /**
  * Created by dima_korolev on 17/06/2017.
@@ -10,14 +22,42 @@ import com.strongpancakes.quest.R
 
 class LoginActivity : AppCompatActivity() {
 
-
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_login)
+        setupBinding()
     }
 
-    override fun onStart() {
-        super.onStart()
+    fun setupBinding() {
+        val validEmail: Observable<Boolean> = RxTextView.textChanges(emailField)
+                .map { Patterns.EMAIL_ADDRESS.matcher(it).matches() }
 
+        val validPassword: Observable<Boolean> = RxTextView.textChanges(passwordField)
+                .map { it.length > 1 }
+
+        val isSignInEnabled: Observable<Boolean> = Observable.combineLatest(validEmail, validPassword, BiFunction {e, p -> e&&p})
+        isSignInEnabled.subscribe(RxView.enabled(loginBtn))
+
+        RxView.clicks(loginBtn).subscribe { login() }
+        RxView.clicks(createAccount).subscribe { startSignup() }
+    }
+
+    fun login() {
+        val auth = OfficeMeApp.instance.auth
+
+        auth.signInWithEmailAndPassword(emailField.text.toString(), passwordField.text.toString())
+                .continueWith {
+                    if (it.isSuccessful) startMain()
+                    else Snackbar.make(findViewById(android.R.id.content), it.exception?.message ?: "Error", Snackbar.LENGTH_LONG).show()
+                }
+    }
+
+    fun startMain() {
+        start(MainActivity::class.java, Intent.FLAG_ACTIVITY_CLEAR_TOP)
+        finish()
+    }
+
+    fun startSignup() {
+        start(CreateAccountActivity::class.java)
     }
 }
--- android/app/src/main/java/com/strongpancakes/quest/utils/Extensions.kt
+++ android/app/src/main/java/com/strongpancakes/quest/utils/Extensions.kt
@@ -2,11 +2,37 @@ package com.strongpancakes.quest.utils
 
 import android.app.Activity
 import android.content.Intent
+import android.view.View
+import android.view.ViewGroup
+import com.strongpancakes.quest.R
 
 /**
  * Created by dima_korolev on 17/06/2017.
  */
 
-fun Activity.start(activity: Class<out Activity>) {
-    startActivity(Intent(this, activity))
+fun Activity.start(activity: Class<out Activity>, vararg flags: Int = intArrayOf()) {
+    val intent = Intent(this, activity)
+    flags.forEach { intent.addFlags(it) }
+    startActivity(intent)
+}
+
+fun Activity.showProgress() {
+    var progressView = getProgressView()
+    if (progressView != null) {
+        progressView.visibility = View.VISIBLE
+    } else {
+        progressView = layoutInflater.inflate(R.layout.item_progress_bar, null)
+        (findViewById(android.R.id.content) as? ViewGroup)?.addView(progressView)
+    }
+}
+
+fun Activity.hideProgress() {
+    getProgressView()?.visibility = View.INVISIBLE
+}
+
+fun Activity.getProgressView(): View? {
+    val rootContainer = (findViewById(android.R.id.content) as ViewGroup)
+    if (rootContainer.childCount == 0) return null
+    val topView = rootContainer.getChildAt(rootContainer.childCount - 1)
+    return if (topView.id == R.id.progressViewScreen) topView else null
 }
